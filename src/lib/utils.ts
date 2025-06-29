6ba0b9da56a7a5250b93905999e6a37085db9ee9
Trilobyte17 <53918738+Trilobyte17@users.noreply.github.com>
2025-06-28 23:01:26 -0700

    Fix utility function imports and organization

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function generateUniqueId(): string {
-  return Math.random().toString(36).substr(2, 9) + Date.now().toString(36);
-}
